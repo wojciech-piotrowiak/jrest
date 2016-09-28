54ba6b5fc0ede174f1a2e62d4ab78d98c49d5d38
Krzysztof Koziol <Krzysztof.Koziol@fisglobal.com>
2016-09-28 23:09:18 +0200

    Zadanie2

---
+++ rest-root/02-plain-http-clt/src/main/java/pl/com/sages/jrest/plainhttp/JavaRESTClient.java
@@ -0,0 +1,54 @@
+package pl.com.sages.jrest.plainhttp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/*
+ * Zadanie:
+ * 1. Zmianić klase, tak aby metoda invoke przyjmowała parametr format daty
+ *    i wywoływała zdalną usługę z odpowiednim parametrem.
+ */
+
+public class JavaRESTClient {
+
+    public static void invoke() {
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL("http://localhost:81/date");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.setDoOutput(false);
+
+            System.out.println("Response Code:");
+            System.out.println(connection.getResponseCode());
+            System.out.println("Response Message:");
+            System.out.println(connection.getResponseMessage());
+            System.out.println("Headers:");
+            System.out.println(connection.getHeaderFields());
+            System.out.println("Response Entity:");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        JavaRESTClient.invoke();
+    }
+
+}
