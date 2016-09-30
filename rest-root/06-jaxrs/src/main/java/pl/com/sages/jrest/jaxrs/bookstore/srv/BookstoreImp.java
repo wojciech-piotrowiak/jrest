18b00ebe4ddb265a4d628f6781907f16fcd1aae4
Your Name <you@example.com>
2016-09-30 14:06:35 +0200

    ''

--- rest-root/06-jaxrs/src/main/java/pl/com/sages/jrest/jaxrs/bookstore/srv/BookstoreImp.java
+++ rest-root/06-jaxrs/src/main/java/pl/com/sages/jrest/jaxrs/bookstore/srv/BookstoreImp.java
@@ -6,9 +6,17 @@ import pl.com.sages.jrest.jaxrs.bookstore.Magazine;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+@Path("/store")
+@Produces({"application/json"})
 public class BookstoreImp implements BookstoreAPI {
 
     private static Collection<Item> bookstore = new ArrayList<>();
@@ -17,7 +25,12 @@ public class BookstoreImp implements BookstoreAPI {
         Book book = new Book();
         book.setIsbn("1");
         book.setTitle("REST is Easy");
+        
+        Book book2 = new Book();
+        book2.setIsbn("3");
+        book2.setTitle("REST is almost Easy");
         bookstore.add(book);
+        bookstore.add(book2);
         Magazine m = new Magazine();
         m.setIsbn("2");
         m.setTitle("Programista");
@@ -25,15 +38,18 @@ public class BookstoreImp implements BookstoreAPI {
     }
 
     @Override
+    @Path("/books")
+    @GET
     public Collection<Book> getBooks() {
-        Collection<Book> books = new ArrayList<>();
-        return books;
+    	return bookstore.stream().filter(item->item instanceof Book).map(item->(Book)item).collect(Collectors.toList());
     }
 
     @Override
+    @Path("/magazines")
+    @Produces("text/plain")
+    @GET
     public Collection<Magazine> getMagazines() {
-        Collection<Magazine> magazines = new ArrayList<>();
-        return magazines;
+    	return bookstore.stream().filter(item->item instanceof Magazine).map(item->(Magazine)item).collect(Collectors.toList());
     }
 
     @Override
@@ -42,13 +58,18 @@ public class BookstoreImp implements BookstoreAPI {
     }
 
     @Override
-    public Response getBook(String id) {
-        Book book = null;
-
-        return null;
+    @Path("/books/{isbn}")
+    @GET
+    public Response getBook(@PathParam("isbn") String id) {
+    	Optional<Item> findFirst = bookstore.stream().filter(item-> (item instanceof Book)&&id.contentEquals(item.getIsbn())).findFirst();
+    	if(findFirst.isPresent())
+		return Response.ok(findFirst.get()).build();
+    	else
+    		return Response.status(404).build();
     }
 
     @Override
+    @Path("/books/{isbn}")
     public Book addBook(String id, String title) {
         Book book = null;
         return book;
